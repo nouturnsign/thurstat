be3c48ce3205145c5e04874831cea2a20c74299f
nouturnsign <ericwangyy@gmail.com>
2022-07-23 15:32:51 -0400

    Rename to demo.py

---
+++ demo.py
@@ -0,0 +1,4 @@
+from thurstat import *
+
+update_config(global_seed=2022)
+X = UniformContinuousDistribution(a=0, b=1, c=2)
--- sandstat.py
+++
@@ -1,6 +0,0 @@
-from thurstat import *
-
-update_config(global_seed=2022)
-X = UniformContinuousDistribution(a=0, b=1)
-print(X.generate_random_values(10))
-print(X.generate_random_values(15))
